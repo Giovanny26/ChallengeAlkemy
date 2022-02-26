54b2c93bbd4153149f60d3dcc871ac2c65ada204
Giovanny26 <81320091+Giovanny26@users.noreply.github.com>
2022-02-26 00:09:39 -0500

    Restoring spoonacular apo ley

--- my-restaurant/src/Service/productService.js
+++ my-restaurant/src/Service/productService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class ProductService{
     baseUrl = "https://api.spoonacular.com/recipes/complexSearch?query="
     search(name){
-        return axios.get(this.baseUrl+name+"&number=5"+"&addRecipeInformation=true"+"&apiKey=7ddd50e8051442bc9a33c4c1e6a2cca4").then(res => res)
+        return axios.get(this.baseUrl+name+"&number=5"+"&addRecipeInformation=true"+"&apiKey=b20138bdbdb34739be553ca5241eefa4").then(res => res)
     }
     nutrition(id){
         return axios.get("https://api.spoonacular.com/food/menuItems/"+id+"/nutritionWidget").then(res => res)
